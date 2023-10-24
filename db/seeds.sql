6a04f360c711b926a546bcbab4b0beec991e0b19
taycannon <taycannon2003@gmail.com>
2023-10-24 15:18:27 -0500

    finished seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,16 @@ VALUES ("Chief financial officer", 250, 1),
        ("IT Lead", 300, 4),
        ("IT staff", 243, 4),
        ("IT Intern", 50, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sharon" "Hoover", 1, 1),
+       ("Carol" "James", 2, null),
+       ("Kate" "Woods", 3, null),
+       ("Barb" "Smith", 4, 2),
+       ("Jacob", 5, null),
+       ("Steve" "Jackson", 6, null),
+       ("Miles" "Jones", 7, 3),
+       ("Nacy" "Miller", 8, null),
+       ("Leo" "Williams", 9, 4),
+       ("Danny" "Wilson", 10, null),
+       ("Laura" "Adams", 11, null);
