ed74ac6b4dd9069369821081e2f64aa56b76a7fd
taycannon <taycannon2003@gmail.com>
2023-10-24 18:35:50 -0500

    Created a MySQL connection

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const mysql = require('mysql2');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 
-con
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Created a MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost', //127.0.0.1 for MAC if localhost is not working
+  user: '',
+  password: '',
+  database: 'employees_db',
+
+},
+console.log(`Connected to the employer_db database.`)
+);
