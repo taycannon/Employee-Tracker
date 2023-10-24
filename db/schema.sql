0202cf033ac30aa80380c30de41e2a873ab4d7ba
taycannon <taycannon2003@gmail.com>
2023-10-24 15:07:14 -0500

    Added code to seed and schema for roles, pay, & id

--- db/schema.sql
+++ db/schema.sql
@@ -14,3 +14,11 @@ title: VARCHAR(30)
 salary: DECIMAL
 department_id: INT
 );
+
+CREATE TABLE employee (
+id: INT PRIMARY KEY
+first_name: VARCHAR(30)
+last_name: VARCHAR(30)
+role_id: INT
+manager_id: INT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES ("Finance"),
+       ("Human Resources"),
+       ("Sales"),
+       ("Technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Chief financial officer", 250, 1),
+       ("Financial planner", 71, 1),
+       ("Portfolio manager", 148, 1),
+       ("Chief Human Resource Officer", 110, 2),
+       ("Staff", 60, 2),
+       ("Staff", 72, 2),
+       ("Sales Lead", 81, 3),
+       ("Salesperson", 38, 3),
+       ("IT Lead", 300, 4),
+       ("IT staff", 243, 4),
+       ("IT Intern", 50, 4);
